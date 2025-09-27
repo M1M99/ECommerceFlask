c01c5339484aabd73083e0f4d5de3e2ebe97f6b2
ASUS <ASUS@DESKTOP-NB7MT4D>
2025-09-28 01:11:52 +0400

    FinishedProductCategory

---
+++ models.py
@@ -0,0 +1,40 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Product(db.Model):
+    __tablename__ = 'products'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(70),nullable=False)
+    price = db.Column(db.Float,default=1)
+    quantity = db.Column(db.Integer,default=1)
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
+    image_url = db.Column(db.String(70),nullable=True)
+    category = db.relationship('Category', back_populates='products')
+
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(20),nullable=False)
+    description = db.Column(db.String(70),nullable=True)
+    products = db.relationship('Product', back_populates='category')
+
+
+class Client(db.Model):
+    __tablename__ = 'clients'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25),nullable=False)
+    surname = db.Column(db.String,nullable=True)
+    email = db.Column(db.String)
+    phone = db.Column(db.String)
+    address = db.Column(db.String)
+    orders = db.relationship('Order', back_populates='client')
+
+
+class Order(db.Model):
+    __tablename__ = 'orders'
+    id = db.Column(db.Integer, primary_key=True)
+    client_id = db.Column(db.Integer, db.ForeignKey('clients.id'))
+    client = db.relationship('Client', back_populates='orders')
